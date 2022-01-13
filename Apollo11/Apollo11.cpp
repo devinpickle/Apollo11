e1a146373d3713acb7ac012b78f859ed62faa645
Collette Stapley <sta18019@byui.edu>
2022-01-13 11:37:24 -0700

    added namespace std

--- Apollo11/Apollo11.cpp
+++ Apollo11/Apollo11.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
